672faec821f617c2a92f3b49661b7924b629988f
Matt Martini <matt.martini@gmail.com>
2024-02-19 14:32:12 -0500

    Import utils from MERM::LogArchive

--- lib/MERM/SmartTools/Utils.pm
+++ lib/MERM/SmartTools/Utils.pm
@@ -4,9 +4,14 @@ use 5.018;
 use strict;
 use warnings;
 
+use YAML::XS qw(LoadFile);
+use Regexp::Parser;
+use File::Temp;
+use Term::ReadKey;
+
 =head1 NAME
 
-MERM::SmartTools::Utils - The great new MERM::SmartTools::Utils!
+MERM::SmartTools::Utils - functions to assist in the testing of MERM::SmartTools
 
 =head1 VERSION
 
@@ -16,37 +21,467 @@ Version 0.01
 
 our $VERSION = '0.01';
 
-
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
+MERM::SmartTools::Utils - provides functions to assist in the testing of MERM::SmartTools.
 
-Perhaps a little code snippet.
 
     use MERM::SmartTools::Utils;
 
-    my $foo = MERM::SmartTools::Utils->new();
-    ...
+
+    my $fexists  = file_exists('/bla/somefile');
+    my $canreadf  = file_readable('/bla/somefile');
+    my $canwritef = file_writeable('/bla/somefile');
+
+    my $fexists  = dir_exists('/somedir');
+    my $canreadf  = dir_readable('/somedir');
+    my $canwritef = dir_writeable('/somedir');
+
+
+=cut
+
+our @ISA = qw(Exporter);
+
+our @EXPORT = qw(
+    mk_temp_dir
+    mk_temp_file
+    prompt
+    yes_no_prompt
+    valid
+    banner
+    file_exists
+    file_readable
+    file_writeable
+    dir_exists
+    dir_readable
+    dir_writeable
+    dir_suffix_slash
+    stat_date
+);
 
 =head1 EXPORT
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
+mk_temp_dir
+mk_temp_file
+prompt
+yes_no_prompt
+valid
+banner
+file_existsd
+file_readable
+file_writeable
+dir_exists
+dir_readabled
+dir_writeable
+dir_suffix_slash
+stat_date
 
-=head1 SUBROUTINES/METHODS
+=head1 SUBROUTINES
 
-=head2 function1
+=head2 mk_temp_dir
+
+Create a temporary directory in tmp for use in testing
 
 =cut
 
-sub function1 {
+sub mk_temp_dir {
+
+    my $temp_dir = File::Temp->newdir( DIR     => '/tmp',
+                                       CLEANUP => 1 );
+
+    return ($temp_dir);
+}    # mk_temp_dir
+
+=head2 mk_temp_file
+
+Create a temporary file in tmp or supplied dir for use in testing
+
+=cut
+
+sub mk_temp_file {
+    my $temp_dir = shift || '/tmp';
+
+    my $temp_file = File::Temp->new( DIR    => $temp_dir,
+                                     SUFFIX => '.test',
+                                     UNLINK => 1
+    );
+
+    print $temp_file 'super blood wolf moon' . "\n";
+
+    return ($temp_file);
+}    # mk_temp_file
+
+=head2 prompt
+
+Prompt user for input
+
+=head3 settings
+
+refhash of settings
+
+=over 4
+
+=item text
+
+text to display
+
+=item default
+
+default value, if any
+
+=item noecho
+
+turn off echo
+
+=item append
+
+string to append after prompt
+
+=item prepend
+
+string to prepern before prompt
+
+=item okempty
+
+empty string is ok
+
+=item valid
+
+ref_list of valid answers or a ref_sub for checking validation.  Sub should return false or $err in case of error
+
+=back
+
+=cut
+
+sub prompt {
+    my ($settings) = @_;
+    my ( $str, $err );
+    my $default = $$settings{default} || '';
+    my $valid
+        = ( defined $$settings{valid} and not ref( $$settings{valid} ) )
+        ? join( ',', @{ $$settings{valid} } )
+        : '';
+    my $msg = $$settings{prepend} . $$settings{text} || '';
+    $msg .= " ($valid)"   if ($valid);
+    $msg .= " [$default]" if ($default);
+    do {
+        print "$msg$$settings{append}";
+
+        ReadMode 'noecho' if ( $$settings{noecho} );
+        $str = <STDIN>;
+        ReadMode 'restore' if ( $$settings{noecho} );
+        chomp $str;
+        $str = lc($str);
+
+        #assign default value if it was provided
+        $str = $default if ( $default && $str eq '' );
+
+        #check if string is valid based on valid responses
+        if ( $err = valid( $str, $$settings{valid}, $$settings{okempty} ) ) {
+            print "$$settings{prepend}$err\n";
+            $str = $err = '';
+        }
+        } while ( $str eq ''
+                  && !( exists $$settings{okempty} && $$settings{okempty} ) );
+
+    print "\n" if ( exists $$settings{noecho} );
+    return $str;
 }
 
-=head2 function2
+=head2 yes_no_prompt
+
+boolean prompt
+
+=head3 settings
+
+refhash of settings
+
+=over 4
+
+=item text
+
+text to displya
+
+=item default
+
+0 --> no, 1 --> yes, undef --> none
+
+=item append
+
+string to append after prompt
+
+=item prepend
+
+string to prepern before prompt
+
+=back
+
+Returns: 1 -- yes, 0 -- no
+=cut
+
+sub yes_no_prompt {
+    my ($settings) = @_;
+    my $str;
+    my $msg = $$settings{prepend} . $$settings{text} || '';
+    if ( exists $$settings{default} && $$settings{default} ) {
+        $msg
+            .= ( $$settings{default} )
+            ? " ([Y]/N)$$settings{append}"
+            : " (Y/[N])$$settings{append}";
+    } else {
+        $msg .= " (Y/N)$$settings{append}";
+    }
+    do {
+        print "$msg";
+        $str = <STDIN>;
+        chomp $str;
+        $str = lc($str);
+        if ( exists $$settings{default} && $$settings{default} ) {
+            $str = ( $$settings{default} ) ? 'y' : 'n' unless ($str);
+        }
+    } while ( $str !~ /[yn]/i );
+    return ( $str =~ /y/i ) ? 1 : 0;
+}
+
+=head2 valid
+
+helper function for the prompt
+
+returns undef if selection is valid , errmsg if error
+
+=head3 Params
+
+=over 4
+
+=item str
+
+user response
+
+=item valid
+
+either ref_array of valid answers or ref_sub that returns true/false
+
+=item okempty
+
+is empty string ok
+
+=back
 
 =cut
 
-sub function2 {
+sub valid {
+    my $str     = shift;
+    my $valid   = shift;
+    my $okempty = shift;
+
+    return unless ($valid);    #no valid options give, so -- true by default
+
+    return if ( !$str && $okempty );
+
+    #valid is a sub ref -- call it
+    return &$valid($str) if ( ref($valid) eq 'CODE' );
+
+    #default -- simply grep for valid reponse in array ref
+    return if grep {/^$str$/i} @$valid;
+
+    return "Invalid choice";
+}
+
+=head2 banner
+
+print a banner
+
+=cut
+
+sub banner {
+    my $banner = shift;
+    my $fh     = shift || \*STDOUT;
+
+    my $width;
+    if ( -t STDOUT ) {
+        ($width) = GetTerminalSize();
+    } else {
+        $width = 80;
+    }
+
+    my $spacer = ( $width - 2 ) - length($banner);
+    my $lspace = int( $spacer / 2 );
+    my $rspace = $lspace + $spacer % 2;
+
+    print $fh "#" x $width . "\n";
+    print $fh "#" . " " x ( $width - 2 ) . "#" . "\n";
+    print $fh "#" . " " x $lspace . $banner . " " x $rspace . "#" . "\n";
+    print $fh "#" . " " x ( $width - 2 ) . "#" . "\n";
+    print $fh "#" x $width . "\n";
+    print $fh "\n";
+
+    return;
+}
+
+=head2 stat_date
+
+return the state date of a file
+format YYYYMMDD, or YYYY/MM/DD if dir_format is true
+or if date_type is monthly
+format YYYYMM or YYYY/MM
+
+=cut
+
+sub stat_date {
+    my $file        = shift;
+    my $dir_format  = shift || 0;
+    my $date_format = shift || 'daily';
+    my ( $date, $format );
+
+    my $mtime = ( stat $file )[9];
+
+    if ( $date_format eq 'monthly' ) {
+        $format = $dir_format ? "%04d/%02d" : "%04d%02d";
+        $date = sprintf(
+            $format,
+            sub { ( $_[5] + 1900, $_[4] + 1 ) }
+                ->( localtime($mtime) )
+        );
+    } else {
+        $format = $dir_format ? "%04d/%02d/%02d" : "%04d%02d%02d";
+        $date = sprintf(
+            $format,
+            sub { ( $_[5] + 1900, $_[4] + 1, $_[3] ) }
+                ->( localtime($mtime) )
+        );
+    }
+    return $date;
+}
+
+=head2 dir_suffix_slash
+
+ensures a dir ends in a slash by adding one if neccessary
+
+=cut
+
+sub dir_suffix_slash {
+    my $dir = shift;
+
+    $dir .= ( substr( $dir, -1, 1 ) eq '/' ) ? '' : '/';
+    return $dir;
+}
+
+=head2 file_exists
+
+tests for file existance
+
+=cut
+
+sub file_exists {
+    my $file = shift;
+
+    if ( -f $file ) {
+        return 1;
+    } else {
+        return 0;
+    }
+    return;
+}
+
+=head2 file_readable
+
+Tests for file existence and is readable
+
+=cut
+
+sub file_readable {
+    my $file = shift;
+
+    unless ( file_exists($file) ) {
+        return 0;
+    }
+
+    if ( -r $file ) {
+        return 1;
+    } else {
+        return 0;
+    }
+    return;
+}
+
+=head2 file_writeable
+
+tests for file existance and is readable and is writeable
+
+=cut
+
+sub file_writeable {
+    my $file = shift;
+
+    unless ( file_readable($file) ) {
+        return 0;
+    }
+
+    if ( -w $file ) {
+        return 1;
+    } else {
+        return 0;
+    }
+    return;
+}
+
+=head2 dir_exists
+
+tests for dir existance
+
+=cut
+
+sub dir_exists {
+    my $dir = shift;
+
+    if ( -d $dir ) {
+        return 1;
+    } else {
+        return 0;
+    }
+    return;
+}
+
+=head2 dir_readable
+
+tests for dir existance and is readable
+
+=cut
+
+sub dir_readable {
+    my $dir = shift;
+
+    unless ( dir_exists($dir) ) {
+        return 0;
+    }
+
+    if ( -r $dir ) {
+        return 1;
+    } else {
+        return 0;
+    }
+    return;
+}
+
+=head2 dir_writeable
+
+tests for dir existance and is readable and is writeable
+
+=cut
+
+sub dir_writeable {
+    my $dir = shift;
+
+    unless ( dir_readable($dir) ) {
+        return 0;
+    }
+
+    if ( -w $dir ) {
+        return 1;
+    } else {
+        return 0;
+    }
+    return;
 }
 
 =head1 AUTHOR
@@ -102,4 +537,4 @@ This is free software, licensed under:
 
 =cut
 
-1; # End of MERM::SmartTools::Utils
+1;    # End of MERM::SmartTools::Utils
