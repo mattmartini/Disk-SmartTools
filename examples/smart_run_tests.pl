daa5791b6e2574b13d281a02fc1de6199438ae9a
Matt Martini <matt.martini@gmail.com>
2024-06-25 12:11:57 -0400

    Increment version number

--- examples/smart_run_tests.pl
+++ examples/smart_run_tests.pl
@@ -25,7 +25,7 @@ use Data::Printer class =>
     { expand => 'all', show_methods => 'none', parents => 0 };
 
 Readonly my $PROGRAM => 'smart_run_tests.pl';
-use version; Readonly my $VERSION => version->declare("v2.0.15");
+use version; Readonly my $VERSION => version->declare("v2.1.3");
 
 ########################################
 #      Define Global Variables         #
